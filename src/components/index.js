40c05939c305fc08c603e77f5927d4638051274a
Akin <>
2022-11-24 19:07:39 +0100

    implement Navbar section

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Clients from "./Clients";
+import ClickAction from "./ClickAction";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import HeroSection from "./HeroSection";
+
+export { Navbar, Billing, CardDeal, Business, Clients, ClickAction, Stats, Footer, Testimonials, HeroSection };
